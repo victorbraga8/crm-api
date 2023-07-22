236aba1aaa4cb9f0d4d65a56333b5d743f9bb334
victorbraga <victor.medeiros@contraprova.com.br>
2023-07-22 17:40:21 -0300

    Update for Fornecedor Model

--- src/controllers/SupplierController.ts
+++ src/controllers/SupplierController.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest } from "fastify";
 import { prisma } from "../lib/prisma";
+import { request } from "http";
 
 interface supplierInterface {
   id: string;
@@ -13,13 +14,78 @@ class SupplierController {
     return suppliers;
   }
 
+  async getSupplier(request: FastifyRequest<{ Params: supplierInterface }>) {
+    const supplier = await prisma.fornecedor.findUniqueOrThrow({
+      where: {
+        id: request.params.id,
+      },
+    });
+
+    return supplier;
+  }
+
   async createSupplier(request: FastifyRequest<{ Body: supplierInterface }>) {
-    // const { nome, dataInicio } = request.body;
-    // request.body.dataInicio = new Date(dataInicio);
     const createSupplier = await prisma.fornecedor.create({
       data: request.body,
     });
     return createSupplier;
+  }
+
+  async updateSupplier(
+    request: FastifyRequest<{ Body: supplierInterface }>,
+    response: any
+  ) {
+    const { id, nome, dataInicio } = request.body;
+
+    try {
+      const supplier = await prisma.fornecedor.findUniqueOrThrow({
+        where: {
+          id: id,
+        },
+      });
+
+      const updateData = {
+        nome: nome,
+        dataInicio: dataInicio,
+      };
+
+      const updateSupplier = await prisma.fornecedor.update({
+        where: {
+          id: id,
+        },
+        data: updateData,
+      });
+      return updateSupplier;
+    } catch (error) {
+      return response
+        .status(404)
+        .send({ msg: "Fornecedor não encontrado ou não existente." });
+    }
+  }
+
+  async deleteSupplier(
+    request: FastifyRequest<{ Params: supplierInterface }>,
+    response: any
+  ) {
+    try {
+      const supplier = await prisma.fornecedor.findUnique({
+        where: {
+          id: request.params.id,
+        },
+      });
+
+      await prisma.fornecedor.delete({
+        where: {
+          id: request.params.id,
+        },
+      });
+
+      return { msg: "Fornecedor Deletado" };
+    } catch (error) {
+      return response
+        .status(404)
+        .send({ msg: "Fornecedor não encontrado ou já deletado" });
+    }
   }
 }
 
--- src/routes/supplier.ts
+++ src/routes/supplier.ts
@@ -1,11 +1,11 @@
 import { FastifyInstance } from "fastify";
 
 const supplierController = require("../controllers/SupplierController");
-
+// Get All Suppliers - Get One Supplier - Create Supplier - Update Supplier
 export async function supplierRoutes(app: FastifyInstance) {
-  app.get("/supplier", supplierController.getAllSuppliers);
-  app.post("/supplier", supplierController.createSupplier);
-  // app.get("/user/:nome", supplierController.getSupplier);
-  // app.put("/user", supplierController.updateSupplier);
-  // app.delete("/user/:id", supplierController.deleteSupplier);
+  app.get("/supplier-find", supplierController.getAllSuppliers);
+  app.get("/supplier-find/:id", supplierController.getSupplier);
+  app.post("/supplier-create", supplierController.createSupplier);
+  app.put("/supplier-update", supplierController.updateSupplier);
+  app.delete("/supplier-delete/:id", supplierController.deleteSupplier);
 }
